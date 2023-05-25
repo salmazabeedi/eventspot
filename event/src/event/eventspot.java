ad1e00565519a84a411375f51c95b87ac8f04f49
wrood <121840460+Wrood1@users.noreply.github.com>
2023-05-25 10:39:40 +0300

    Update eventspot.java "قبل الباترن "

--- event/src/event/eventspot.java
+++ event/src/event/eventspot.java
@@ -1,39 +1,36 @@
 /*
-package event;
-
-/**
- *
- * @author 96656
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
+package event20;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import static java.sql.DriverManager.println;
 import java.util.Scanner;
 import org.json.simple.parser.ParseException;
+/**
+ *
+ * @author w
+ */
+
 
 public class eventspot {
 
-    /**
-     * @param args the command line arguments
-     *
-     */
     public static void main(String[] args) throws IOException, FileNotFoundException, ParseException {
         // TODO code application logic here
-        
-        welcomeMethod();
         int choice = 0;
         boolean repeat = true;
         boolean validEmail = false;
         boolean validpass = false;
         String type = null;
         int choose;
-        boolean validPhoneNum=false;
         String firstName, lastName, userName, phoneNumber, Email, password, repass;
 
-        userandpass user1 = new userandpass();
+        //userandpass user1 = new userandpass();
 
         do {
-            
+            System.out.println("          __Event spot__ ");
             System.out.println("1-sign in");
             System.out.println("2-create account");
             System.out.println("3-exit");
@@ -50,8 +47,11 @@ public class eventspot {
                     String userName0 = keyboard.nextLine();
                     System.out.println("please enter your password:");
                     String password0 = keyboard.nextLine();
+                            userandpass user11 = new userandpass(userName0,password0);
 
-                    if (user1.checkLogin(userName0, password0)) {
+    
+                            //userandpass user1 = new userandpass();
+  if (user11.checkLogin()) {
 
                        eventPlace1 place=new eventPlace1(); 
                     } else {
@@ -66,71 +66,49 @@ public class eventspot {
                     System.out.println("Please enter your personal information:");
 
                     System.out.print("First name: ");
-                    firstName = input.nextLine().trim();
-                    while (!firstName.matches("[a-zA-Z]+")) {
-                    System.out.print("\nInvalid first name. Please enter only letters for the name \nFirst name: ");
-                    firstName = input.nextLine().trim();
-                    }
+                    firstName = input.nextLine();
 
                     System.out.print("Last name: ");
-                    lastName = input.nextLine().trim();
-                    while (!lastName.matches("[a-zA-Z]+")) {
-                    System.out.print("\nInvalid last name. Please enter only letters for the name \nLast name: ");
-                    lastName = input.nextLine().trim();
-                    }
+                    lastName = input.nextLine();
                     System.out.print("User name: ");
                     userName = input.nextLine();
+                    System.out.print("Phone number: ");
+                    phoneNumber = input.nextLine();
+                    
+                    
+
                     do {
-                    System.out.print("Phone number (starts with 05 and has a length of 10 digits): ");
-                    phoneNumber = input.nextLine().trim();
+                        System.out.print("Email address: ");
+                        Email = input.nextLine();
 
-                    if (phoneNumber.matches("05\\d{8}")) {
-                       validPhoneNum = true;
-                    } else if (!phoneNumber.matches("\\d+")) {
-                    System.out.println("Invalid phone number format. Please enter only digits.");
-                    } else if (!phoneNumber.startsWith("05")) {
-                    System.out.println("Invalid phone number format. Please enter a number that starts with 05.");
-                    } else {
-                    System.out.println("Invalid phone number length. Please enter 10 digits.");
-                    } 
-                    } while (!validPhoneNum);
-
-                   do {
-                   System.out.print("Email address: ");
-                   Email = input.nextLine();
-
-                    if (Email.matches("^[\\w.-]+@[\\w.-]+\\.[A-Za-z]{2,}$")) {
-                    validEmail = true;
-                    } else {
-                    System.out.println("Invalid email address format. Please enter an email address that contains only letters, digits, '.' and '@' characters, and is in the format '*****@**.*'. For example, 'john.doe@example.com'");
-                    }
+                        if (Email.contains("@")) {
+                            validEmail = true;
+                        } else {
+                            System.out.println("Invalid email address. Please try again.");
+                        }
                     } while (!validEmail);
 
                     // cheak pass
                     do {
-                    System.out.print("Please enter a password (at least 6 characters, letters and digits only): ");
-                    password = input.nextLine();
+                        System.out.print("Please enter a password: ");
+                        password = input.nextLine();
 
-                    System.out.print("Please re-enter the password: ");
-                    repass = input.nextLine();
+                        System.out.print("Please re-enter the password: ");
+                        repass = input.nextLine();
 
-                    if (repass.equals(password)) {
-                    if (password.matches("^[a-zA-Z0-9]{6,}$")) {
-                    validpass = true;
-                    } 
-                    else {
-                    System.out.println("Invalid password format. Please enter a password that has at least 6 characters and contains only letters or digits.");
-                    }
-                    }
-                    else {
-                    System.out.println("Passwords do not match. Please try again.");
-                    }
+                        if (repass.equals(password)) {
+                            System.out.println("Passwords match!");
+                            validpass = true;
+                        } else {
+                            System.out.println("Passwords do not match. Please try again.");
+
+                        }
                     } while (!validpass);
 
                     System.out.println("please enter your city:");
                     String city = keyboard.nextLine();
 
-                    user1.creat(userName, password);
+                //    user1.creat(userName, password);
                     user u1 = new user(firstName, lastName, phoneNumber, Email, city, userName, password, repass);
 
                     break;
@@ -140,20 +118,5 @@ public class eventspot {
             }
 
         } while (repeat);
-    }
-    
-     public static void welcomeMethod(){
-       
-        System.out.println("-----------------------------------------------------------------------------------");
-        System.out.println();
-        System.out.println("-----------------------------------------------------------------------------------"); 
-        System.out.println();
-        System.out.println("          *************   welcome to EventSpot app  **************  \n "
-                         + "EventSpot is an application where you can find the perfect spot for your event");
-        System.out.println();
-        System.out.println("-----------------------------------------------------------------------------------"); 
-        System.out.println();
-        System.out.println("-----------------------------------------------------------------------------------"); 
-     
     }
 }
